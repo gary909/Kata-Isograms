861d62ecbe00d061a02d43cb5a9affa9d887f990
gary <gary.white@lodgit.com>
2022-07-26 13:16:13 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+// make lowercase, then split into individual letters
+// in array sort in Alphabetical order.  Then compare
+// for a match.
 function isIsogram(str) {
-    //...
+    let myStr = str.toLowerCase();
+    let myArr = myStr.split("").sort();
+    for (let i = 0; i < str.length; i++) {
+        if (myArr[i] == myArr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 
