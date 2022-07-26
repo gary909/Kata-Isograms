6428ff8e6a2cbc0d2ef96c023254fc9cc2f24ca3
gary <gary.white@lodgit.com>
2022-07-26 12:54:32 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function isIsogram(str) {
+    //...
+}
+
+
+console.log(isIsogram("Dermatoglyphics")); // True
+console.log(isIsogram("isogram")); // True
+console.log(isIsogram("aba")); // False
+console.log(isIsogram("moOse")); // False
